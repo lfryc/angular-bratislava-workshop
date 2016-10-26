400f834283816cc4a1790fff1b7b85f821379e35
Lukas Fryc <lukas@fryc.eu>
2016-10-26 18:19:48 +0200

    Refactor service from JS to TS

--- src/app/services/todoList.ts
+++ src/app/services/todoList.ts
@@ -1,28 +1,14 @@
-angular
-  .module('app.services', [])
-  .service('todoList', TodoList);
+class TodoList {
 
+  list;
+  filteredList;
+  filterState = 'all';
 
-function TodoList() {
+  constructor() {
+    this.filteredList = this.list = [];
+  }
 
-  this.filterState = 'all';
-  this.filteredList = this.list = [];
-
-  this.add = add;
-  this.toggleAll = toggleAll;
-  this.toggleStatus = toggleStatus;
-  this.remove = remove;
-  this.clearCompleted = clearCompleted;
-  this.showCompleted = showCompleted;
-  this.showActive = showActive;
-  this.showAll = showAll;
-  this.$filter = $filter;
-  this.countPending = countPending;
-  this.countCompleted = countCompleted;
-  this.hasTasks = hasTasks;
-  this.$refreshList = $refreshList;
-
-  function add(description) {
+  add(description) {
     var task = new Task(description);
     this.list.push(task);
     this.$refreshList();
@@ -30,7 +16,7 @@ function TodoList() {
     return task;
   }
 
-  function toggleAll() {
+  toggleAll() {
     const complete = this.countPending() !== 0;
     this.list = this.list.map(function (task) {
       task.complete = !!complete;
@@ -40,67 +26,66 @@ function TodoList() {
     this.$filter();
   }
 
-  function toggleStatus(task) {
+  toggleStatus(task) {
     task.complete = !task.complete;
     this.$filter();
   }
 
-  function remove(item) {
+  remove(item) {
     this.list = this.list.filter(function (todo) {
       return todo !== item
     });
     this.$filter();
   }
 
-  function clearCompleted() {
+  clearCompleted() {
     this.list = this.list.filter(function (todo) {
       return !todo.complete
     });
     this.$filter();
   }
 
-  function showCompleted() {
+  showCompleted() {
     this.filterState = 'completed';
     this.$filter();
   }
 
-  function showActive() {
+  showActive() {
     this.filterState = 'active';
     this.$filter();
   }
 
-  function showAll() {
+  showAll() {
     this.filterState = 'all';
     this.$filter();
   }
 
-  function $filter(filterState) {
-    this.filterState = filterState || this.filterState;
-    const showAll = ('all' === this.filterState);
-    const showCompleted = 'completed' === this.filterState;
+  $filter(filterState = this.filterState) {
+    const showAll = ('all' === filterState);
+    const showCompleted = 'completed' === filterState;
 
     this.filteredList = this.list.filter(function (item) {
       return (showAll || showCompleted === item.complete)
     });
   }
 
-  function countPending() {
+  countPending() {
     return this.list.filter(function (item) {
       return !item.complete
     }).length;
   }
 
-  function countCompleted() {
+  countCompleted() {
     return this.list.filter(function (item) {
       return item.complete;
     }).length;
   }
 
-  function hasTasks() {
+  hasTasks() {
     return 0 !== this.list.length;
   }
 
-  function $refreshList() {
+  $refreshList() {
     this.$filter();
   }
 
@@ -114,11 +99,14 @@ function Task(description) {
   function guid() {
     function s4() {
       return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
+          .toString(16)
+          .substring(1);
     }
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-      s4() + '-' + s4() + s4() + s4();
+        s4() + '-' + s4() + s4() + s4();
   }
 }
 
+angular
+    .module('app.services', [])
+    .service('todoList', TodoList);
