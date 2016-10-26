19ca8db1bc9949a79e508ed27f0a4e0732cdc15f
Lukas Fryc <lukas@fryc.eu>
2016-10-26 17:54:43 +0200

    Refactor index.html references to main.ts/vendor.ts/polyfills.ts leveraging webpack

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+
+import 'normalize.css';
+import 'todomvc-app-css/index.css';
+
+import './app/services/services';
+import './app/services/todoList';
+import './app/components/todoTextInput/todoTextInput';
+import './app/components/todoTextInput/autofocus.directive';
+import './app/components/todoTextInput/escape.directive';
+import './app/components/todoTextInput/todoTextInput.component';
+import './app/components/components';
+import './app/components/todoBatchToggle.component';
+import './app/components/todoFooter.component';
+import './app/components/todoItem.component';
+import './app/components/todoListFilter.component';
+import './app/app';
+import './app/app.component';
+import './app/app.bootstrap';
